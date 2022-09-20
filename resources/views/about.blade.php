71da959eca75acd48ebdbadf0f700e78468363be
markajKING <mark.hobbel@live.nl>
2022-09-20 12:41:42 +0200

    New Page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/web')
+@section('headTitle', $headTitle)
+@section('content')
+    <h1>{{$headTitle}}</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'show']);
+Route::get('/about', [AboutController::class, 'show']);
 
